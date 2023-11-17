24273e91292766a1b1c82ed3e4060012d034c360
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-11-17 16:51:54 -0300

    Criação da model Medida.

---
+++ src/main/java/org/example/models/Medida.java
@@ -0,0 +1,101 @@
+package org.example.models;
+
+public class Medida {
+    private Long id;
+    private double cintura;
+    private double torax;
+    private double bracoDireito;
+    private double bracoEsquerdo;
+    private double coxaDireita;
+    private double coxaEsquerda;
+    private double panturrilhaDireita;
+    private double panturrilhaEsquerda;
+
+
+    public Medida(Long id, double cintura, double torax, double bracoDireito, double bracoEsquerdo, double coxaDireita, double coxaEsquerda, double panturrilhaDireita, double panturrilhaEsquerda) {
+        this.id = id;
+        this.cintura = cintura;
+        this.torax = torax;
+        this.bracoDireito = bracoDireito;
+        this.bracoEsquerdo = bracoEsquerdo;
+        this.coxaDireita = coxaDireita;
+        this.coxaEsquerda = coxaEsquerda;
+        this.panturrilhaDireita = panturrilhaDireita;
+        this.panturrilhaEsquerda = panturrilhaEsquerda;
+    }
+
+    public Medida() {}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getCintura() {
+        return cintura;
+    }
+
+    public void setCintura(double cintura) {
+        this.cintura = cintura;
+    }
+
+    public double getTorax() {
+        return torax;
+    }
+
+    public void setTorax(double torax) {
+        this.torax = torax;
+    }
+
+    public double getBracoDireito() {
+        return bracoDireito;
+    }
+
+    public void setBracoDireito(double bracoDireito) {
+        this.bracoDireito = bracoDireito;
+    }
+
+    public double getBracoEsquerdo() {
+        return bracoEsquerdo;
+    }
+
+    public void setBracoEsquerdo(double bracoEsquerdo) {
+        this.bracoEsquerdo = bracoEsquerdo;
+    }
+
+    public double getCoxaDireita() {
+        return coxaDireita;
+    }
+
+    public void setCoxaDireita(double coxaDireita) {
+        this.coxaDireita = coxaDireita;
+    }
+
+    public double getCoxaEsquerda() {
+        return coxaEsquerda;
+    }
+
+    public void setCoxaEsquerda(double coxaEsquerda) {
+        this.coxaEsquerda = coxaEsquerda;
+    }
+
+    public double getPanturrilhaDireita() {
+        return panturrilhaDireita;
+    }
+
+    public void setPanturrilhaDireita(double panturrilhaDireita) {
+        this.panturrilhaDireita = panturrilhaDireita;
+    }
+
+    public double getPanturrilhaEsquerda() {
+        return panturrilhaEsquerda;
+    }
+
+    public void setPanturrilhaEsquerda(double panturrilhaEsquerda) {
+        this.panturrilhaEsquerda = panturrilhaEsquerda;
+    }
+}
